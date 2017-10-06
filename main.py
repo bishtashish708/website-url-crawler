62aec754f410d302687005c2b086f0df8200a09d
bishtashish708 <bishtashish708@gmail.com>
2017-10-07 02:50:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import threading
+from queue import Queue
+from crawler import spider
+from crawler import domain
+from crawler import general
+#from spider import Spider
+#from domain import *
+#from general import *
+from crawler.domain import get_domain_name
+from crawler.general import file_to_set
+from crawler.spider import Spider
+
+PROJECT_NAME = 'yahoo'
+HOMEPAGE = 'https://in.yahoo.com/?p=us'
+DOMAIN_NAME = get_domain_name(HOMEPAGE)
+QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+NUMBER_OF_THREADS = 8
+queue = Queue()
+Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+
+
+# Create worker threads (will die when main exits)
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+
+# Do the next job in the queue
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done()
+
+
+# Each queued link is a new job
+def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+    queue.join()
+    crawl()
+
+
+# Check if there are items in the queue, if so crawl them
+def crawl():
+    queued_links = file_to_set(QUEUE_FILE)
+    if len(queued_links) > 0:
+        print(str(len(queued_links)) + ' links in the queue')
+        create_jobs()
+
+
+create_workers()
+crawl()
